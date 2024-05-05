2d417111f4ee9a1c1be4cc4e65f45f257ac332d4
Filip Kubicz <filip.kubicz@tier.app>
2024-05-05 12:05:18 +0200

    command: Add units tests and help

--- src/command.rs
+++ src/command.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum CommandError {
     ArgError,
     NotSupported,
@@ -17,25 +17,26 @@ impl fmt::Display for CommandError {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Command {
     Add(String),
     Remove,
     Start,
     Stop,
     Manage,
+    Help, // handled by top level main
 }
 
 impl Command {
     pub fn new(args: &[String]) -> Result<Self, CommandError> {
-        let command_type = args[1].clone();
-
         if args.len() < 2 {
             // commands need type
             return Err(CommandError::ArgError);
         }
 
+        let command_type = args[1].clone();
         match command_type.as_str() {
+            "help" => Ok(Command::Help),
             "add" => {
                 // add command also needs payload
                 if args.len() < 3 {
@@ -49,5 +50,45 @@ impl Command {
             "manage" => Ok(Command::Manage),
             _ => Err(CommandError::NotSupported),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let args = [
+            String::from("target/debug/pomodorino"),
+            String::from("add"),
+            String::from("Task 1"),
+        ];
+        assert_eq!(Command::new(&args), Ok(Command::Add("Task 1".to_string())));
+    }
+
+    #[test]
+    fn test_add_no_arg() {
+        let args = [String::from("target/debug/pomodorino"), String::from("add")];
+        assert_eq!(Command::new(&args), Err(CommandError::ArgError));
+    }
+
+    #[test]
+    fn test_too_little_arg() {
+        let args = [String::from("target/debug/pomodorino")];
+        assert_eq!(Command::new(&args), Err(CommandError::ArgError));
+    }
+
+    #[test]
+    fn test_rm() {
+        let args = [String::from("pomodorino"), String::from("rm")];
+        assert_eq!(Command::new(&args), Ok(Command::Remove));
+    }
+
+    #[test]
+    fn test_not_supported() {
+        let args = [String::from("pomodorino"), String::from("flytothemoon")];
+        assert_eq!(Command::new(&args), Err(CommandError::NotSupported));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 // Plan: implement basic function as quickly as possible, then refactor, move to separate files.
-// 1. CLI accepts commands:
+// 1. CLI accepts commands: DONE
 // - add
 // - rm
 // - start
 // - stop
 // - manage -> list -> done/undo
-// 2. Have tests for the command creation
+// 2. Have tests for the command creation DONE
 // 3, Adding them to db, iterate with tests for adding to db
 // 4. Timer - iterate with tests for timers and moving to done
 // 5. Improve displaying
@@ -23,6 +23,9 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
     match Command::new(&args) {
+        Ok(Command::Help) => {
+            display_help();
+        }
         Ok(command) => {
             info!("Command: {:?}, args: {:?}", command, args);
             // Add to DB
@@ -31,4 +34,9 @@ fn main() {
             error!("error when creating a Command: {:?}", e);
         }
     }
+}
+
+fn display_help() {
+    println!("Usage: pomodorino <command> [task string]");
+    println!("add command requires payload, others don't");
 }
