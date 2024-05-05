a378c945cb07f2c37b6f381720e6a4d33d757e9f
Filip Kubicz <filip@kubicz.engineer>
2024-05-05 14:19:50 +0200

    db: Use native_db to store Task

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,97 @@ use log::{error, info};
 use simple_logger::SimpleLogger;
 use std::env;
 
+// DB start
+use native_db::*;
+use native_model::{native_model, Model};
+use serde::{Deserialize, Serialize};
+
+// #[derive(Copy, Clone, Serialize, Deserialize, PartialEq, Debug)]
+// //#[native_db]
+// enum TaskState {
+//     Todo,
+//     Done,
+// }
+
+// impl InnerKeyValue for TaskState {
+//     fn database_inner_key_value(&self) -> db_type::DatabaseInnerKeyValue {
+//         db_type::DatabaseInnerKeyValue::new(vec![*self as u8])
+//     }
+// }
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[native_model(id = 1, version = 1)]
+#[native_db]
+pub struct Task {
+    #[primary_key]
+    name: String,
+    #[secondary_key]
+    state: String, // using String for now, switch to enum when native_db issue resolved
+}
+
+impl Task {
+    pub fn new(name: String) -> Self {
+        Task {
+            name,
+            state: String::from("todo"), // TaskState::Todo,
+        }
+    }
+}
+
+pub fn db_init(builder: &mut DatabaseBuilder) -> Result<Database, db_type::Error> {
+    // Initialize the model
+    builder.define::<Task>()?;
+    let db = builder.create_in_memory()?;
+
+    Ok(db)
+}
+pub fn db_add(db: &Database, name: String) -> Result<(), db_type::Error> {
+    info!("db_add: {name}");
+
+    let rw = db.rw_transaction().unwrap();
+    rw.insert(Task::new(name))?;
+    rw.commit()?;
+    info!("transaction committed!");
+    Ok(())
+}
+pub fn db_read_all(db: &Database) -> Result<Vec<Task>, db_type::Error> {
+    // Read all tasks
+    // Open a read-only transaction
+    let r = db.r_transaction()?;
+    // Iterate items with name starting with "red"
+    let values: Vec<Task> = r.scan().primary()?.all().collect();
+    Ok(values)
+}
+pub fn db_rm() {}
+pub fn db_set_done() {}
+pub fn db_set_todo() {}
+// DB end
+
 fn main() {
     info!("Pomodorino start");
     SimpleLogger::new().init().unwrap();
 
     let args: Vec<String> = env::args().collect();
+
+    // db init
+    let mut builder = DatabaseBuilder::new();
+    let db = db_init(&mut builder).unwrap();
+
     match Command::new(&args) {
         Ok(Command::Help) => {
             display_help();
         }
+        Ok(Command::Add(task_name)) => {
+            db_add(&db, task_name).unwrap();
+            let tasks = db_read_all(&db).unwrap();
+
+            info!("db tasks: {:?}", tasks);
+        }
         Ok(command) => {
-            info!("Command: {:?}, args: {:?}", command, args);
-            // Add to DB
+            info!(
+                "Command not implemented yet: {:?}, args: {:?}",
+                command, args
+            );
         }
         Err(e) => {
             error!("error when creating a Command: {:?}", e);
